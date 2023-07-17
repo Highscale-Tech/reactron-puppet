eef3f2af0f264cf34a241c71c14ba02771653db6
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-07-17 15:36:38 +0100

    Update icon

--- public/electron.js
+++ public/electron.js
@@ -11,6 +11,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, "icon.png"),
     autoHideMenuBar: true,
     // Set the path of an additional "preload" script that can be used to
     // communicate between node-land and browser-land.
